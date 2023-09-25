20a0c22ce04c72188ab4e18e383350b7a34c5d19
Michael Mussato <michimussato@gmail.com>
2023-09-25 10:22:10 +0200

    move 2in1screen to dedicated repo

--- 2in1screen_init_screen.c
+++ 2in1screen_init_screen.c
@@ -1,4 +1,4 @@
-// mkdir -p ./bin && gcc -O2 -o ./bin/2in1screen 2in1screen.c && sudo mv ./bin/2in1screen /usr/bin/
+// mkdir -p ./bin && gcc -O2 -o ./bin/2in1screen_init_screen 2in1screen_init_screen.c && sudo mv ./bin/2in1screen_init_screen /usr/bin/
 
 /*
 Installation:
@@ -95,71 +95,57 @@ double accel_gx = 2.0;
 double accel_gy = 6.0;
 
 double thrshld = 8.0;
-int current_state = 0;
 
-int rotation_changed(){
-	int state = 0;
-
-	fprintf(stdout, "x %f ", accel_x);
-	fprintf(stdout, "gx %f ", accel_gx);
-	fprintf(stdout, "y %f ", accel_y);
-	fprintf(stdout, "gy %f ", accel_gy);
-
-	if(-1*thrshld < accel_x && accel_x < thrshld && -1*thrshld < accel_y && accel_y < thrshld)
-	{
-		// change direction only if i want to
-		fprintf(stdout, "not changed\n");
-		return 0;
-	}
-
-	if(accel_y < -accel_gy)
-	{
-		state = 0;
-	}
-	else if(accel_y > accel_gy)
-	{
-		state = 1;
-	}
-#if N_STATE == 4
-	else if(accel_x > accel_gx)
-	{
-		state = 2;
-	}
-	else if(accel_x < -accel_gx)
-	{
-		state = 3;
-	}
-
-	fprintf(stdout, "state %i\n", state);
-#endif
-
-	if(current_state!=state)
-	{
-		current_state = state;
-		return 1;
-	}
-	else return 0;
-}
+int state = 0;
 
 FILE* bdopen(char const *fname, char leave_open){
-	*basedir_end = '/';
-	strcpy(basedir_end+1, fname);
-	FILE *fin = fopen(basedir, "r");
-	setvbuf(fin, NULL, _IONBF, 0);
-	fgets(content, DATA_SIZE, fin);
-	*basedir_end = '\0';
+    *basedir_end = '/';
+    strcpy(basedir_end+1, fname);
+    FILE *fin = fopen(basedir, "r");
+    setvbuf(fin, NULL, _IONBF, 0);
+    fgets(content, DATA_SIZE, fin);
+    *basedir_end = '\0';
 
-	if(leave_open==0)
-	{
-		fclose(fin);
-		return NULL;
-	}
-	else return fin;
+    if(leave_open==0)
+    {
+        fclose(fin);
+        return NULL;
+    }
+    else return fin;
 }
 
+
+
 void rotate_screen(){
+    fprintf(stdout, "x %f ", accel_x);
+    fprintf(stdout, "gx %f ", accel_gx);
+    fprintf(stdout, "y %f ", accel_y);
+    fprintf(stdout, "gy %f ", accel_gy);
+
+    if(accel_y < -accel_gy)
+    {
+        state = 0;
+    }
+    else if(accel_y > accel_gy)
+    {
+        state = 1;
+    }
+    #if N_STATE == 4
+    else if(accel_x > accel_gx)
+    {
+        state = 2;
+    }
+    else if(accel_x < -accel_gx)
+    {
+        state = 3;
+    }
+
+    fprintf(stdout, "state %i\n", state);
+    #endif
+
+
     // ROTATE DISPLAY
-	sprintf(command, "xrandr --output %s --rotate %s", output, ROT[current_state]);
+	sprintf(command, "xrandr --output %s --rotate %s", output, ROT[state]);
 	system(command);
 
 	// ADJUST SCROLL TOUCH SCREEN
@@ -173,11 +159,11 @@ void rotate_screen(){
 	// sprintf(command, "xinput set-prop \"%s\" --type=float \"Coordinate Transformation Matrix\" %s", "GXTP7380:00 27C6:0113 Stylus stylus", COOR[current_state]);
 	// sprintf(command, "xinput set-prop \"%s\" --type=float \"Coordinate Transformation Matrix\" %s", "GXTP7380:00 27C6:0113 Stylus eraser", COOR[current_state]);
 	// libwacom
-	sprintf(command, "xsetwacom set \"%s\" \"Rotate\" %s", "GXTP7380:00 27C6:0113 touch", ROT_WACOM[current_state]);
+	sprintf(command, "xsetwacom set \"%s\" \"Rotate\" %s", "GXTP7380:00 27C6:0113 touch", ROT_WACOM[state]);
 	system(command);
-	sprintf(command, "xsetwacom set \"%s\" \"Rotate\" %s", "GXTP7380:00 27C6:0113 Stylus stylus", ROT_WACOM[current_state]);
+	sprintf(command, "xsetwacom set \"%s\" \"Rotate\" %s", "GXTP7380:00 27C6:0113 Stylus stylus", ROT_WACOM[state]);
 	system(command);
-	sprintf(command, "xsetwacom set \"%s\" \"Rotate\" %s", "GXTP7380:00 27C6:0113 Stylus eraser", ROT_WACOM[current_state]);
+	sprintf(command, "xsetwacom set \"%s\" \"Rotate\" %s", "GXTP7380:00 27C6:0113 Stylus eraser", ROT_WACOM[state]);
 	system(command);
 	sleep(1.0);  // needed, otherwise the mapping happens too early, resulting in a wrong one
 	sprintf(command, "xsetwacom set \"%s\" \"MapToOutput\" %s", "GXTP7380:00 27C6:0113 touch", output);
@@ -190,7 +176,7 @@ void rotate_screen(){
 
     // ADJUST TRANSFORM MATRIX TOUCHPAD
     // libinput (only?)
-	sprintf(command, "xinput set-prop \"%s\" --type=float \"Coordinate Transformation Matrix\" %s", "HAILUCK CO.,LTD USB KEYBOARD Mouse", COOR_TOUCHPAD[current_state]);
+	sprintf(command, "xinput set-prop \"%s\" --type=float \"Coordinate Transformation Matrix\" %s", "HAILUCK CO.,LTD USB KEYBOARD Mouse", COOR_TOUCHPAD[state]);
 	system(command);
 }
 
@@ -220,24 +206,20 @@ int main(int argc, char const *argv[]) {
 	double scale = atof(content);
 
 	FILE *dev_accel_y = bdopen("in_accel_y_raw", 1);
-#if N_STATE == 4
-	FILE *dev_accel_x = bdopen("in_accel_x_raw", 1);
-#endif
+    #if N_STATE == 4
+    FILE *dev_accel_x = bdopen("in_accel_x_raw", 1);
+    #endif
 
-	while(1)
-	{
-		fseek(dev_accel_y, 0, SEEK_SET);
-		fgets(content, DATA_SIZE, dev_accel_y);
-		accel_y = atof(content) * scale;
-#if N_STATE == 4
-		fseek(dev_accel_x, 0, SEEK_SET);
-		fgets(content, DATA_SIZE, dev_accel_x);
-		accel_x = atof(content) * scale;
-#endif
-		if(rotation_changed())
-			rotate_screen();
-		sleep(2);
-	}
-	
+    fseek(dev_accel_y, 0, SEEK_SET);
+    fgets(content, DATA_SIZE, dev_accel_y);
+    accel_y = atof(content) * scale;
+    #if N_STATE == 4
+    fseek(dev_accel_x, 0, SEEK_SET);
+    fgets(content, DATA_SIZE, dev_accel_x);
+    accel_x = atof(content) * scale;
+    #endif
+
+    rotate_screen();
+
 	return 0;
 }
